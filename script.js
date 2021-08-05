1a5aa128addb4d86d0f7444159953a1e06600e63
phurin prasit <phurin@web1.dara.ac.th>
2021-08-05 14:45:35 +0700

    v1

--- script.js
+++ script.js
@@ -3,11 +3,52 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const search = document.querySelector('#search')
+const text = document.getElementById('text')
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  const x = "640631102 PANNAWIT PANWONG"
+  const y = 640631102 + parseInt(length.value)
+  if (btn_toggle.innerHTML == "Show Calculation") {
+    author.innerHTML = y
+    btn_toggle.innerHTML = "Show Author"
+  }
+  else {
+    author.innerHTML = x
+    btn_toggle.innerHTML = "Show Calculation"
+  }
+}
+
+search.onclick = () => {
+  const myArr = text.innerHTML.split(" ")
+  var stringOut = ""
+  // console.log(myArr)
+  for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i].length >= length.value) {
+      // stringOut += "<mark>" + myArr[i] + "</mark>"
+      
+      console.log(myArr[i])
+    }
+    else {
+      stringOut += myArr[i] + " ";
+    }
+  }
+  document.getElementById("text").innerHTML = stringOut;
+
 }
 
 // more codes for Search and Reset buttons here
+
+
+
+// for (let i = 0; i < myArr.length; i++) {
+//   if (myArr[i].length >= length) {
+//     stringOut += "<mark>" + myArr[i] + "</mark>"
+//   }
+//   else {
+//     stringOut += myArr[i] + " ";
+//   }
+// }
+// document.getElementById("text").innerHTML = stringOut;
